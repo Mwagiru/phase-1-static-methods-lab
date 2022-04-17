efe4b674b86648d29daf8b1dfb288beebddb3dd2
Mwagiru <joymwagiru@gmail.com>
2022-04-17 21:53:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ class Formatter {
     return string.replace(/[^A-Za-z0-9 '-]/g, ''); 
   }
   static titleize(string){
-    
+   let exceptions = ["the","a","an","but","of","and","for","at","by","from"];
+  let newArr=[]
+   let result=string.split( " " )
+    for (let i=0;i<result.length;i++){
+      if(i==0){
+        newArr.push(this.capitalize(result[i]))
+      }else{
+        if(exceptions.includes(result[i])){
+          newArr.push(result[i])
+        }else{
+          newArr.push(this.capitalize(result[i]))
+        }
+      }
+    }
+    return newArr.join( " " );
   }
 }
